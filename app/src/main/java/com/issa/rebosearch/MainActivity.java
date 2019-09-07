6ed356c6836b40c60b72142420d1058fe59d354e
issa12345 <issaalhiali@gmail.com>
2019-09-07 21:42:20 +0300

    update

--- app/src/main/java/com/issa/rebosearch/MainActivity.java
+++ app/src/main/java/com/issa/rebosearch/MainActivity.java
@@ -1,10 +1,15 @@
 package com.issa.rebosearch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText mSearchBoxText;
@@ -26,5 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         //this is id for url result
         mSearchResult = (TextView)findViewById(R.id.tv_gitub_result_json);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search){
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
